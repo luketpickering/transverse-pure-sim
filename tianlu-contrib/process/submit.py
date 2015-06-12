e4a8c2b9f2face209ca7aba0e7089d36c09ebdcb
Tianlu Yuan <tianlutheman@gmail.com>
2015-06-12 16:22:14 -0600

    Added author and email info and made loop slighlty better in submit_tgts

--- tianlu-contrib/process/submit.py
+++ tianlu-contrib/process/submit.py
@@ -8,15 +8,19 @@ from thirdparty.elements import ELEMENTS
 from builder import Builder
 
 
+__author__ = 'Tianlu Yuan'
+__email__ = 'tianlu.yuan [at] colorado.edu'
+
+
 def submit_tgts(tgts=[6, 8, 18, 26, 29, 30, 82]):
     builder = Builder()
     submittor = submit()
 
     for tgt in tgts:
+        builder.tgt = ELEMENTS[tgt]
+        builder.uniquepath = os.path.join('targets', builder.tgt.symbol)
         for run in range(100):
             builder.runnum = run
-            builder.tgt = ELEMENTS[tgt]
-            builder.uniquepath = os.path.join('targets', builder.tgt.symbol)
 
             submittor.add_job(builder.build_genie())
             submittor.add_job(builder.build_neut())
