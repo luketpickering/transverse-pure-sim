bda67b93df83d4064a203d1306e5e6204ee08990
Tianlu Yuan <tianlutheman@gmail.com>
2015-06-16 11:51:18 -0600

    refactored input/output dir into a single variable

--- tianlu-contrib/process/transversevars.sh
+++ tianlu-contrib/process/transversevars.sh
@@ -34,12 +34,15 @@ for generator in "${generators[@]}"
 do
     for category in "${categories[@]}"
     do
-        output="${HOME}/store/other/generators/${generator}/${category}/TransverseVars.root"
+        inputdir="${HOME}/store/other/generators/${generator}/${category}"
+        input="${inputdir}/*.root"
+        output="${inputdir}/TransverseVars.root"
+
         if [ "${force}" = true ]; then
             test -f  ${output} && echo "Removing ${output} to allow overwrite!" && rm -f ${output}
         fi
 
-        ProcessRooTrackerToTransversityVariables.exe -i "${HOME}/store/other/generators/${generator}/${category}/*.root" -o ${output}  -g ${generator}
+        ProcessRooTrackerToTransversityVariables.exe -i "${input}" -o ${output}  -g ${generator}
 
     done
 done
