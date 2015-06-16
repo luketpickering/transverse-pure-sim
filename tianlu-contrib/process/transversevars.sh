fe2c4a113c351e7ed2575f7398e12d85dc06b35c
Tianlu Yuan <tianlutheman@gmail.com>
2015-06-16 11:07:43 -0600

    added command line option to force removal of TransverseVars.root to allow reprocessing

--- tianlu-contrib/process/transversevars.sh
+++ tianlu-contrib/process/transversevars.sh
@@ -3,14 +3,36 @@
 # Converts rootracker output to transverse variable output with Luke's
 # ProcessRooTrackerToTransversityVariables.exe
 
-
 generators=( 'neut' 'genie' )
 categories=( 'targets/C' 'targets/O' 'targets/Ar' 'targets/Fe' 'targets/Cu' 'targets/Zn' 'targets/Pb' )
+
+# Parse command line options
+force=false
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -f|--force)
+            force=true
+            shift
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
 
 for generator in "${generators[@]}"
 do
     for category in "${categories[@]}"
     do
-        ProcessRooTrackerToTransversityVariables.exe -i "${HOME}/store/other/generators/${generator}/${category}/*.root" -o ${HOME}/store/other/generators/${generator}/${category}/TransverseVars.root -g ${generator}
+        output="${HOME}/store/other/generators/${generator}/${category}/TransverseVars.root"
+        if [ "${force}" = true ]; then
+            test -f  ${output} && echo "Removing ${output} to allow overwrite!" && rm -f ${output}
+        fi
+
+        ProcessRooTrackerToTransversityVariables.exe -i "${HOME}/store/other/generators/${generator}/${category}/*.root" -o ${output}  -g ${generator}
+
     done
 done
