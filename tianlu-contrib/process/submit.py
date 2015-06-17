cdd0c771fc17c761bb90c6170ad581561a249c68
Tianlu Yuan <tianlutheman@gmail.com>
2015-06-17 14:32:18 -0600

    Added options to submit jobs for individual or multiple generators at a time

--- tianlu-contrib/process/submit.py
+++ tianlu-contrib/process/submit.py
@@ -11,18 +11,69 @@ __author__ = 'Tianlu Yuan'
 __email__ = 'tianlu.yuan [at] colorado.edu'
 
 
+class SubmitGenerator(submit):
+    def __init__(self):
+        """Inherits from class submit. Incorporates additional commandline
+        args
+        """
+        super(SubmitGenerator, self).__init__()
+        self.parser.add_option('--neut',
+                               dest='neut',
+                               action='store_true',
+                               default=False,
+                               help='process NEUT')
+
+        self.parser.add_option('--genie',
+                               dest='genie',
+                               action='store_true',
+                               default=False,
+                               help='process GENIE')
+
+        self.parser.add_option('--nuwro',
+                               dest='nuwro',
+                               action='store_true',
+                               default=False,
+                               help='process NUWRO')
+
+        self.parser.add_option('--gibuu',
+                               dest='nuwro',
+                               action='store_true',
+                               default=False,
+                               help='process GIBUU')
+
+        self.parser.add_option('--allgen',
+                               dest='allgen',
+                               action='store_true',
+                               default=False,
+                               help='process all generators')
+
+
+def add_gen_jobs(builder, submittor):
+    """Checks the CL options and decide which generator jobs to add
+    """
+    (options, args) = submittor.parser.parse_args()
+    if options.genie:
+        submittor.add_job(builder.build_genie())
+    if options.neut:
+        submittor.add_job(builder.build_neut())
+    if options.nuwro:
+        submittor.add_job(builder.build_nuwro())
+    if options.allgen:
+        submittor.add_job(builder.build_genie())
+        submittor.add_job(builder.build_neut())
+        submittor.add_job(builder.build_nuwro())
+
+
 def submit_tgts(tgts=[6, 8, 18, 26, 29, 30, 82]):
     builder = Builder()
-    submittor = submit()
+    submittor = SubmitGenerator()
 
     for tgt in tgts:
         builder.tgt = ELEMENTS[tgt]
         builder.category = ['targets', builder.tgt.symbol]
         for run in range(100):
             builder.runnum = run
-
-            submittor.add_job(builder.build_genie())
-            submittor.add_job(builder.build_neut())
+            add_gen_jobs(builder, submittor)
 
     submittor.run()
 
