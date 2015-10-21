19b52a9b019ae6e1afaf24fbe58eafa59b36c121
Luke Pickering <luketpickering@googlemail.com>
2015-10-21 08:13:19 +0100

    Reorganises CLITools interface.
      - Adds Argument addition interface which filters unexpected options
        and returns them in a new'd char * [] so that this parser can
        be used in conjunction with others.
    
      - Options can no longer be added directly to the option list but
        instead are created by a perfect argument forwarding template.
        Proffered options are checked for compatibility before adding
        to the list to preclude bugs where multiple options could respond
        to the same invocation.
    
      - Attempts to implement some move-semantics type code. We will see
        how well it works...

--- luke-contrib/utils/CLITools.cxx
+++ luke-contrib/utils/CLITools.cxx
@@ -1,58 +1,134 @@
+#include <set>
+#include <numeric>
+#include <utility>
+
 #include "CLITools.hxx"
 
 namespace CLIArgs {
 std::vector<Option> OptSpec;
 std::vector<std::string> Args;
 
-  Option::Option(){
-    ShortName = "";
-    LongName = "";
-    HasVal = false;
-    Required = false;
-    Used = false;
-    CallBack = [](std::string const &) -> bool { return false; };
-    Default = [](){};
+Option::Option(){
+  IsValid = false;
+}
+
+Option::Option(
+  std::string shortname,
+  std::string longname,
+  bool hasval,
+  std::function<bool(std::string const &opt)> callback,
+  bool required,
+  std::function<void()> def,
+  std::string valString){
+
+  ShortName = shortname;
+  LongName = longname;
+  HasVal = hasval;
+  CallBack = callback;
+  Required = required;
+  Default = def;
+  ValueIdent = valString;
+  Used = false;
+  IsValid = true;
+}
+
+Option::Option(Option &from){
+
+  ShortName = from.ShortName;
+  LongName = from.LongName;
+  ValueIdent = from.ValueIdent;
+
+  HasVal = from.HasVal;
+  CallBack = from.CallBack;
+  Required = from.Required;
+  Default = from.Default;
+  Used = from.Used;
+  IsValid = from.IsValid;
+}
+
+Option::Option(Option &&from) :
+  ShortName( std::move(from.ShortName) ),
+  LongName( std::move(from.LongName) ),
+  ValueIdent( std::move(from.ValueIdent) ){
+
+  HasVal = from.HasVal;
+  CallBack = from.CallBack;
+  Required = from.Required;
+  Default = from.Default;
+  Used = from.Used;
+  IsValid = from.IsValid;
+}
+
+bool Option::IsOpt(std::string const &optname) const{
+  return ((optname == ShortName) || (optname == LongName));
+}
+
+std::ostream& operator<<(std::ostream& os, Option const &opt){
+  return os << "(" << opt.ShortName << "|" << opt.LongName << ")"
+    << (opt.HasVal?" " + opt.ValueIdent:std::string(""))
+    << (opt.Required?" [Required]":"");
+}
+
+Option GetOpt(std::string const &profopt){
+  for(auto &opt : OptSpec){
+    if(!opt.IsOpt(profopt)) {
+      continue;
+    }
+    return opt;
   }
+  return Option();
+}
 
-  Option::Option(
-    std::string shortname,
-    std::string longname,
-    bool hasval,
-    std::function<bool(std::string const &opt)> callback,
-    bool required,
-    std::function<void()> def,
-    std::string valString){
-
-    ShortName = shortname;
-    LongName = longname;
-    HasVal = hasval;
-    CallBack = callback;
-    Required = required;
-    Default = def;
-    ValueIdent=valString;
-    Used=false;
-  }
-
-  bool Option::IsOpt(std::string const &optname) const{
-    return ((optname == ShortName) || (optname == LongName));
-  }
-
-  std::ostream& operator<<(std::ostream& os, Option const &opt){
-    return os << "(" << opt.ShortName << "|" << opt.LongName << ")"
-      << (opt.HasVal?" " + opt.ValueIdent:std::string(""))
-      << (opt.Required?" [Required]":"");
-  }
-
-
+void AddHelp(){
+  AddOpt("-h","--help", false,
+  [&] (std::string const &opt) -> bool {
+    SayRunLike();
+    exit(0);
+  });
+}
 
 void AddArguments(int argc, char const * argv[]){
+  AddHelp();
   for(int i = 0; i < argc; ++i){
     Args.emplace_back(argv[i]);
   }
 }
 
-bool GetOpts(){
+void AddArgumentsAndGetUnexpecteds(int argc, char * argv[], int& retArgc,
+  char ** &retArgv){
+  AddHelp();
+  std::set<int> Unexpecteds;
+  //The first argument is the invocation command, we always want this.
+  Args.emplace_back(argv[0]);
+  for(int i = 1; i < argc; ++i){
+
+    Option const & opt = GetOpt(argv[i]);
+    if(!opt){
+      //if we didn't find it, then it is 'unexpected and should be handed back.'
+      Unexpecteds.insert(i);
+      continue;
+    }
+    Args.emplace_back(argv[i]);
+    if(opt.HasVal){
+      ++i;
+      Args.emplace_back(argv[i]);
+    }
+  }
+  //Anyone else will also, likely, always want this.
+  retArgv = new char *[Unexpecteds.size()+1];
+  retArgc = 1;
+  retArgv[0] = argv[0];
+
+  //Add other unexpecteds to the out array.
+  for(const int & ux : Unexpecteds){
+    retArgv[retArgc] = argv[ux];
+    retArgc++;
+  }
+}
+
+bool HandleArgs(){
   bool skipNextArg = false;
+
   for(auto arg_it = std::next(Args.cbegin());
     arg_it < Args.cend(); std::advance(arg_it,1)){
 
@@ -60,51 +136,55 @@ bool GetOpts(){
 
     std::string const & arg = *arg_it;
 
-    bool found = false;
-    for(auto &opt : OptSpec){
-      if(!opt.IsOpt(arg)) { continue; }
-      found = true;
-      if(opt.HasVal){
-        skipNextArg = true;
-        auto next_it = std::next(arg_it);
-        if(next_it == Args.cend()){
-          std::cout << "Didn't find expected value for Option " << opt.LongName
-            << std::endl;
-          return false;
-        }
+    Option opt = GetOpt(arg);
 
-        std::string const & val = *next_it;
-
-        if(!opt.CallBack(val)){
-          std::cout << "Error Option: " << opt.LongName
-            << ": doesn't understand value: \"" << val << "\"" << std::endl;
-          return false;
-        }
-
-      } else {
-        if(!opt.CallBack("")){ //C++11 lambdas cant have default arguments
-          std::cout << "Invalid option: " << opt.LongName << std::endl;
-          return false;
-        }
-      }
-      opt.Used = true;
-    }
-    if(!found){
-      std::cout << "Unknown Option: " << arg << std::endl;
+    if(!opt){
+      std::cout << "Unexpected Option: " << arg << std::endl;
       return false;
     }
-  }
-  bool AllGood = true;
-  for(auto const &opt : OptSpec){
-    if(opt.Required && !opt.Used){
-      std::cout << "Didn't find and option for " << opt.LongName << std::endl;
-      AllGood = false;
+
+    if(opt.HasVal){
+      skipNextArg = true;
+      auto next_it = std::next(arg_it);
+      if(next_it == Args.cend()){
+        std::cout << "Didn't find expected value for Option " << opt.LongName
+          << std::endl;
+        return false;
+      }
+
+      std::string const & val = *next_it;
+
+      if(!opt.CallBack(val)){
+        std::cout << "Error Option: " << opt.LongName
+          << ": doesn't understand value: \"" << val << "\"" << std::endl;
+        return false;
+      }
+
+    } else {
+      if(!opt.CallBack("")){ //C++11 lambdas cant have default arguments
+        std::cout << "Invalid option: " << opt.LongName << std::endl;
+        return false;
+      }
     }
+    opt.Used = true;
+  }
+
+  for(auto const &opt : OptSpec){
     if(!opt.Used){
       opt.Default();
     }
   }
-  return AllGood;
+
+  //Check that all that should have been used, were used.
+  return std::accumulate(OptSpec.begin(),OptSpec.end(), true,
+  [] (bool const &acc, Option const & opt) -> bool {
+    if(!acc){ return acc; }
+    if(opt.Required && !opt.Used){
+      std::cout << "Didn't find an option for " << opt.LongName << std::endl;
+      return false;
+    }
+    return true;
+  });
 }
 
 std::string GetArg(size_t i){ return Args[i]; }
@@ -129,6 +209,10 @@ void SayRunLike(){
   for(auto const & opt: OptSpec){
     std::cout << "[Arg]: " << opt << std::endl;
   }
+}
+
+void _PushOption(Option &&opt){
+  OptSpec.push_back( std::move(opt) );
 }
 
 }
--- luke-contrib/utils/CLITools.hxx
+++ luke-contrib/utils/CLITools.hxx
@@ -9,17 +9,30 @@
 
 namespace CLIArgs {
 
+///Describes a CLI option.
 struct Option {
+  ///Shorter name usable to invoke on the command line.
   std::string ShortName;
+  ///Longer name usable to invoke on the command line.
   std::string LongName;
+  ///Value type description used in usage output.
   std::string ValueIdent;
+  ///Whether this option is followed by a value.
   bool HasVal;
+  ///Whether this option must appear for successful configuration.
   bool Required;
+  ///Whether this option has been used.
   bool Used;
+  ///The function to invoke when handling this option.
   std::function<bool(std::string const &opt)> CallBack;
+  ///The function to invoke when this option was not used.
   std::function<void()> Default;
+  ///Whether this instance is a valid option.
+  bool IsValid;
 
   Option();
+  Option(Option &from);
+  Option(Option &&from);
   Option(std::string shortname,
     std::string longname,
     bool hasval,
@@ -33,22 +46,62 @@ struct Option {
   friend std::ostream& operator<<(std::ostream& os, Option const &opt);
 };
 
-void AddArguments(int argc, char const * argv[]);
-
-bool GetOpts();
-
-std::string GetArg(size_t i);
-size_t GetNArg();
-
-extern std::vector<Option> OptSpec;
-//I feel that this should work, but it doesn't seem to.
-//You can just do OptSpec.emplace_back manually.
-template<class... arguments>
-void AddToOptSpec(arguments&&... args){
-    OptSpec.emplace_back(&args...);
+inline bool operator!(Option const & opt){
+  return (!opt.IsValid);
 }
 
+///Adds all passed in arguments to the argument list
+void AddArguments(int argc, char const * argv[]);
+///\brief Adds known arguments and values to argument list, returns array of
+///unexpected arguments for external use.
+///
+///\note The zeroth entry of argv is added to both the internal argument list
+/// and the returned array of unexpecteds.
+void AddArgumentsAndGetUnexpecteds(int argc, char * argv[], int& retArgc,
+  char ** &retArgv);
+
+///Invokes options for found arguments and defaults for unused options.
+bool HandleArgs();
+
+///Returns the value of a specific argument.
+std::string GetArg(size_t i);
+///Returns the number of arguments that have been added.
+size_t GetNArg();
+
+///Writes usage options to stdout.
 void SayRunLike();
+
+///Attempts to get an option by either ShortName or LongName.
+///
+///Returns a default option if none is found.
+Option GetOpt(std::string const &profopt);
+
+///IMPLEMENTATION:Function which adds a new option to the list of known options.
+///
+///\note This function is not for external use.
+///\todo If AddOpt were not a template then this could be moved to the
+///implementation file.
+void _PushOption(Option &&opt);
+
+///Checks the validity of and a new CLIArgs::Option.
+///
+///Parameter pack is expanded into the CLIArgs::Option constructor so for
+///relevant arguments, see there.
+///returns true if option was added.
+template<class ... Args>
+bool AddOpt(Args... args){
+  Option newopt(args...);
+
+  if(!!GetOpt(newopt.ShortName) || !!GetOpt(newopt.LongName)){
+    std::cout << "[ERROR]: Attempting to add option: " << newopt << ", however "
+    " the " << ((!!GetOpt(newopt.ShortName))?" Short name: " + newopt.ShortName:
+                                             " Long name: " + newopt.LongName)
+    << " is already in use by: " << GetOpt(newopt.LongName) << std::endl;
+    return false;
+  }
+  _PushOption(std::move(newopt));
+  return true;
+}
 
 }
 #endif
