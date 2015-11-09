51d8d72ff67524a693011d43b006d84943ad3352
Luke Pickering <lp208@ic.ac.uk>
2015-11-09 22:18:15 +0000

    GetSubmodules configures local git status to be more verbose.

--- GetSubmodules.sh
+++ GetSubmodules.sh
@@ -3,3 +3,4 @@ git submodule init
 git submodule update
 git submodule foreach --recursive git submodule init
 git submodule foreach --recursive git submodule update
+git config status.submodulesummary 1
